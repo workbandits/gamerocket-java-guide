4affe711e2554935b9fd44444738697a3efe9950
Franck Anso <franck.anso@workbandits.com>
2013-06-11 17:23:11 +0200

    update spark version + add run action guide

---
+++ 2_run_action/src/main/java/com/workbandits/gamerocket/guide/App.java
@@ -0,0 +1,79 @@
+package com.workbandits.gamerocket.guide;
+
+import com.workbandits.gamerocket.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import org.apache.commons.io.FileUtils;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+public class App {
+
+    private static GameRocketGateway gateway = new GameRocketGateway(
+            Environment.DEVELOPMENT,
+            "b76a0c95125649dbb2eeab5713794e60",
+            "b59637c9427b4950bd0db43ca86a0645");
+
+    private static String renderHtml(String pageName) {
+        try {
+            return FileUtils.readFileToString(new File(pageName));
+        } catch (IOException e) {
+            return "Couldn't find " + pageName;
+        }
+    }
+
+    public static void main(String[] args) {
+        get(new Route("/") {
+
+            @Override
+            public Object handle(Request request, Response response) {
+                response.type("text/html");
+                return renderHtml("views/form.html");
+            }
+        });
+
+        post(new Route("/create_customer") {
+
+            @Override
+            public Object handle(Request request, Response response) {
+                PlayerRequest playerRequest = new PlayerRequest()
+                        .name(request.queryParams("name"))
+                        .locale(request.queryParams("locale"));
+
+                Result<Player> result = gateway.player().create(playerRequest);
+
+                response.type("text/html");
+                if (result.isSuccess()) {
+                    // Create the customer in your database with a field which contains Player ID for next calls
+
+                    return "<h1>Success! Player ID: " + result.getTarget().getId() + "</h1>";
+                } else {
+                    return "<h1>Error: " + result.getErrorDescription() + "</h1>";
+                }
+            }
+        });
+        
+        get(new Route("/run_action") {
+ 
+            @Override
+            public Object handle(Request request, Response response) {
+                ActionRequest actionRequest = new ActionRequest()
+                    .player("b76a0c95125649dbb2eeab5713794e60_51b71daf84aeef96233aa468")
+                    .parameters(request.queryMap().toMap());
+
+                Result<Map<String, Object>> result = gateway.action().run("hello-world", actionRequest);
+
+                response.type("text/html");
+                if (result.isSuccess()) {
+                    return "<h1>Success! Result: " + ((Map) result.getTarget().get("data")).get("hello");
+                } else {
+                    return "<h1>Error: " + result.getErrorDescription() + "</h1>";
+                }
+            }
+        });
+    }
+}
